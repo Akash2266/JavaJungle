42298ada3f5c55599789abec8ae06e0cbdf4bd6d
Akash2266 <akash0104006@gmail.com>
2025-09-27 19:13:12 +0530

    Delete src/Week2_Assignment directory

--- src/Week2_Assignment/Problem6.java
+++
@@ -1,8 +0,0 @@
-package Week2_Assignment;
-
-public class Problem6 {
-    public static void main(String[] args) {
-        for (int i=1; i<=15; i++)
-            System.out.print(i*i +" ");
-    }
-}
