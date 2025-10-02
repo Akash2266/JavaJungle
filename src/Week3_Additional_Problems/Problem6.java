de1f0ec727381190490f36b9762f97d9601fdd22
Akash N <akash0104006@gmail.com>
2025-10-02 10:51:56 +0530

    Additional set of problems for week-3

---
+++ src/Week3_Additional_Problems/Problem6.java
@@ -0,0 +1,40 @@
+package Week3_Additional_Problems;
+
+public class Problem6 {
+    public static void main(String[] args) {
+          int[][] mat = { {1, 3, 5},
+                          {7, 9, 11},
+                          {13, 15, 17}
+                         };
+          int target = 9;
+          int column = mat[0].length;
+
+        for (int[] ints : mat) {
+
+            if (target >= ints[0] && target <= ints[column - 1]) {
+                System.out.println(binarySearch(ints, target));
+                System.exit(0);
+            }
+
+        }
+        System.out.println("false");
+
+    }
+        static boolean binarySearch (int[] nums, int target){
+                int left=0, right= nums.length-1, mid;
+
+                   while (left <= right){
+                          mid = left+right/2;
+
+                            if (nums[mid] == target)
+                                  return true;
+
+                            else if (target > nums[mid])
+                                left++;
+
+                            else
+                                right--;
+                   }
+                     return false;
+        }
+}
---
+++ src/Week3_Additional_Problems/Problem7.java
@@ -0,0 +1,46 @@
+package Week3_Additional_Problems;
+
+public class Problem7 {
+    public static void main(String[] args) {
+        int[][] mat = { {1, 1, 1},
+                        {1, 0, 1},
+                        {1, 1, 1}
+                       };
+
+              for (int i=0; i< mat.length; i++){
+                    for (int j=0; j<mat.length; j++){
+                          if (mat[i][j] == 0){
+                              markRow (mat[i]);
+                              markcolumn (mat, j);
+                          }
+
+                    }
+              }
+
+                 for (int i=0; i<mat.length; i++){
+                       for (int j=0; j< mat.length; j++){
+                           if (mat[i][j] == -1)
+                               mat[i][j] = 0;
+                       }
+                 }
+
+                 for (int i=0; i< mat.length; i++){
+                      for (int j=0; j< mat.length; j++){
+                          System.out.print(mat[i][j] +" ");
+                      }
+                     System.out.println();
+                 }
+    }
+       static void markRow (int[] mat){
+         for (int i=0; i< mat.length; i++){
+                if (mat[i] != 0)
+                    mat[i]= -1;
+         }
+       }
+          static void markcolumn (int[][] mat, int j){
+              for (int i=0; i< mat.length; i++){
+                  if (mat[i][j] != 0)
+                      mat[i][j] = -1;
+              }
+          }
+}
