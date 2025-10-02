ae5c2cc2847ef28c09af06ae1b4e6169bc0d7239
Akash N <akash0104006@gmail.com>
2025-10-02 10:52:27 +0530

    Additional set of problems for week-3

---
+++ src/Week3_Additional_Problems/Problem10.java
@@ -0,0 +1,41 @@
+package Week3_Additional_Problems;
+
+public class Problem10 {
+    public static void main(String[] args) {
+        String s1 = "babad";
+        String subString = "", lps="";
+        int maxLength=0;
+
+           for (int i=0; i<s1.length(); i++){
+                 for (int j=i; j<s1.length(); j++){
+                        subString = s1.substring(i, j+1);
+
+                           if (isPalindrome(subString) && subString.length()>maxLength){
+                               maxLength = subString.length();
+                               lps = subString;
+                           }
+
+                 }
+           }
+        System.out.println(lps);
+    }
+       static boolean isPalindrome (String string){
+             char[] subString = string.toCharArray();
+             int left=0, right=subString.length-1;
+             char temp;
+                  while (left < right){
+                         temp = subString[left];
+                         subString[left] = subString[right];
+                         subString[right] = temp;
+                         left++;
+                         right--;
+           }
+                  String str = new String(subString);
+
+                    if (string.equals(str))
+                        return true;
+
+                      else
+                          return false;
+       }
+}
