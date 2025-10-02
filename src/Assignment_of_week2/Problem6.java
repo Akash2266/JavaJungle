b4391abded8afb294ddb7edde0137d02b914c967
Akash N <akash0104006@gmail.com>
2025-10-02 12:49:33 +0530

    Assignment for week-3

---
+++ src/Assignment_of_week2/Problem6.java
@@ -0,0 +1,8 @@
+package Assignment_of_week2;
+
+public class Problem6 {
+    public static void main(String[] args) {
+        for (int i=1; i<=15; i++)
+            System.out.print(i*i +" ");
+    }
+}
